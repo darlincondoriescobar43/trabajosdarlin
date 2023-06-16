09c5def9f315cb4ad7e6dee3ea0d82ee4f5fe88a
darlincondoriescobar43 <136096823+darlincondoriescobar43@users.noreply.github.com>
2023-06-16 10:19:02 -0500

    Update 3.2.cpp

--- 3.2.cpp
+++ 3.2.cpp
@@ -5,16 +5,24 @@ using namespace std;
 int main() {
     int num, doble, triple;
 
-    cout << "Ingrese un numero entero: ";
+    cout << "Ingrese un número entero: ";
     cin >> num;
 
     doble = num * 2;
     triple = num * 3;
 
-    cout << "El doble del numero ingresado es: " << doble << endl;
-    cout << "El triple del numero ingresado es: " << triple << endl;
+    cout << "El doble del número ingresado es: " << doble << endl;
+    cout << "El triple del número ingresado es: " << triple << endl;
+
+    if (doble > 10) {
+        num = num * num * num;
+        cout << "El doble del número ingresado es mayor que 10. El número elevado al cubo es: " << num << endl;
+    }
+
+    if (triple < 4) {
+        num = num * 2;
+        cout << "El triple del número ingresado es menor que 4. El número duplicado es: " << num << endl;
+    }
 
     return 0;
 }
-//  MODIFICAR SI EL DOBLE DEL NUMERO INGRESADO ES MAYOR QUE 10 ENTONCES ELEVAR AL CUBO ESE NUMERO
-// MODIOFICAR SI EL TRIPLE DEL NUMERO INGRESADO ES MENOR QUE 4 ENTONCES DUPLICAR ESE NUMERO
